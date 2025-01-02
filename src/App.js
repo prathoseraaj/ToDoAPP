ccde65dc9b86c02899d293250b0b6cf446383718
prathoseraaj V <146630697+prathoseraaj0312@users.noreply.github.com>
2025-01-02 18:39:30 +0530

    feat: added css style

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false)
   return (
     <div className="App">
       <h1>My ToDos</h1>
-      <div className='todo-input'>
-        <div className='label-item'>
-          <label htmlFor="text">Title</label>
-          <input type="text" placeholder="What's Your Plan"/>
+      <div className='todopage'>
+        <div className='todo-input'>
+          <div className='label-item'>
+            <label htmlFor="text">Title</label>
+            <input type="text" placeholder="What's Your Plan"/>
+          </div>
+          <div className='label-item'>
+            <label htmlFor="text">Description</label>
+            <input type="text" placeholder="About"/>
+          </div>
+          <div className='label-item'>
+            <br />
+            <button className='my-button' type='button'>ADD</button>
+          </div>
         </div>
-        <div className='label-item'>
-          <label htmlFor="text">Description</label>
-          <input type="text" placeholder="About"/>
+        <div className='buttons'>
+          <button className={`sec-button ${isCompleteScreen === false && 'active'}`} onClick={setIsCompleteScreen === false }>TODO</button>
+          <button className={`sec-button ${isCompleteScreen === true && 'active'}`} onClick={setIsCompleteScreen === true }>Completed</button>
         </div>
-        <div className='label-item'>
-          <button id='my-button' type='button'>ADD</button>
-        </div>
-      </div>
-      <div className='buttons'>
-        <button>TODO</button>
-        <button>Completed</button>
-      </div>
-      <div className='todolist-area'>
-        <div>
-          <h3>Task 1</h3>  {/*dummy test*/}
-          <p>description</p>  {/*dummy test*/}
+        <div className='todolist-area'>
+          <div>
+            <h3>Task 1</h3>  {/*dummy test*/}
+            <p>description</p>  {/*dummy test*/}
+          </div>
         </div>
       </div>
     </div>
