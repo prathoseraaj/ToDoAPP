7baeba8172ebc10a7192d08229f2d636d1d573af
prathoseraaj V <146630697+prathoseraaj0312@users.noreply.github.com>
2025-01-03 11:12:59 +0530

    feat: add the todos in the todosarea

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import { useState } from 'react';
+import { use, useState } from 'react';
 import './App.css';
 import { AiFillDelete } from "react-icons/ai";
 import { BsCheckLg } from "react-icons/bs";
 
 function App() {
-  const [isCompleteScreen, setIsCompleteScreen] = useState(false)
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [newtitle, setNewtitle] = useState('');
+  const [newdescription, setNewdiscription] = useState('');
+
+  let handleAddtodos = () => {
+    let newTodoItems = {
+      title: newtitle,
+      description: newdescription
+    }
+    let updateTodos = [...todos]
+    updateTodos.push(newTodoItems)
+    setTodos(updateTodos)
+    
+  }
+
   return (
     <div className="App">
       <h1>My ToDos</h1>
@@ -12,15 +27,21 @@ function App() {
         <div className='todo-input'>
           <div className='label-item'>
             <label htmlFor="text">Title</label>
-            <input type="text" placeholder="What's Your Plan"/>
+            <input type="text" 
+                   placeholder="What's Your Plan"
+                   value={newtitle}
+                   onChange={(e) => {setNewtitle(e.target.value)}}/>
           </div>
           <div className='label-item'>
             <label htmlFor="text">Description</label>
-            <input type="text" placeholder="About"/>
+            <input type="text" 
+                   placeholder="About"
+                   value={newdescription}
+                   onChange={(e) => {setNewdiscription(e.target.value)}}/>
           </div>
           <div className='label-item'>
             <br />
-            <button className='my-button' type='button'>ADD</button>
+            <button className='my-button' type='button' onClick={handleAddtodos}>ADD</button>
           </div>
         </div>
         <div className='buttons'>
@@ -29,16 +50,19 @@ function App() {
           <button className={`sec-button ${isCompleteScreen === true && 'active'}`} 
                   onClick={() => setIsCompleteScreen(true) }>Completed</button>
         </div>
-        <div className='todolist-area'>
-          <div className='intodoarea'>
-            <h2 className='toadd'>Task 1</h2>  {/*dummy test*/}
-            <p className='toadd' id='description'>description</p>  {/*dummy test*/}
+        {todos.map((item,index) => {
+          return(
+          <div className='todolist-area'>
+          <div className='intodoarea' key={index}>
+            <h2 className='toadd'>{item.title}</h2>  {/*dummy test*/}
+            <p className='toadd' id='description'>{item.description}</p>  {/*dummy test*/}
           </div>
           <div className='intodoarea' id='iconss'>
-          <AiFillDelete className='icons' id='deleteicon'/>
-          <BsCheckLg className='icons' id='tickicon'/>
+          <AiFillDelete className='icons' id='deleteicon' onClick={handleDeletebutton}/>
+          <BsCheckLg className='icons' id='tickicon' onClick={handleTickbutton}/>
           </div>
         </div>
+        )})}
       </div>
     </div>
   );
